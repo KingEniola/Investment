3e431cff2282e0bd72cc2cb7110f7e1880d90ae2
Yetunde Adegbegha <46171364+KingEniola@users.noreply.github.com>
2022-09-14 17:00:20 -0400

    Update InvestmentClient.java

--- InvestmentClient.java
+++ InvestmentClient.java
@@ -1,22 +1,20 @@
 /**
- * Yetunde Eniola Adegbegha
+ * Eniola Adegbegha
  * March 1 2022
  */
 import java.text.*;
 public class InvestmentClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		 Investment person1 = new Investment();           //call default constructor
-	    Investment person2 = new Investment("Eniola", 1000 );          //call overload constructor 
+		 Investment person1 = new Investment();           
+	    Investment person2 = new Investment("Eniola", 1000 );          
 	     
 	     DecimalFormat icPattern = new DecimalFormat("$#,###.0#");
 	     DecimalFormat irPattern = new DecimalFormat("#.0#%");
 
-	  // Test Mutator
-	     person1.setInvestorName("Eniola");                
-	     person1.setInvestment(1000);
+	     person1.setInvestorName("Eni");                
+	     person1.setInvestment(100);
 
 	     System.out.println("                    Welcome to Future Value Investment Calculation");
 	  // Test Accessors
@@ -27,11 +25,10 @@ public class InvestmentClient {
 	     System.out.println("\t Investment in 5 years: " + icPattern.format(person1.futurevalue(5)));             
 	     System.out.println("\t Investment in 10 years: " + icPattern.format(person1.futurevalue(10)));         
 	     System.out.println("\t Investment in 20 years: " + icPattern.format(person1.futurevalue(20)));          
-	     //Test toString()
-	     System.out.println("Investor 1 investment state is: " + person1.toString());         //explicit call toString()
+	   
 	     System.out.println("Investor 1 investment state is: " + person1);               //implicit call toString()
 
-	     //Test Accessors
+	    
 	     String investorname = person2.getInvestorName();
 	     System.out.println("Investor 2: " + investorname);
 	     
@@ -44,11 +41,11 @@ public class InvestmentClient {
 	     System.out.println("\t Investment in 5 years: " + icPattern.format(person2.futurevalue(5)));
 	     System.out.println("\t Investment in 10 years: " + icPattern.format(person2.futurevalue(10)));
 	     System.out.println("\t Investment in 20 years: " + icPattern.format(person2.futurevalue(20)));
-	   //Test toString()
+		
 	     System.out.println("Investor 2 investment state is: " + person2.toString());      //explicit call toString()
-	     System.out.println("Investor 2 investment state is: " + person2);           //implicit call toString()
+	    
 	          
-	     // Test equals()
+	     // equals()
 	     if (person1.equals(person2))
 				System.out.println("Investor 1 and Investor 2 are in the same state");
 			else
